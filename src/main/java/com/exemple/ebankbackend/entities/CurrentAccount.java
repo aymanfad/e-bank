5c3f525dba6b55598811a0a7e637841a38ba8eef
aymanfad <97688907+aymanfad@users.noreply.github.com>
2024-06-29 13:09:03 +0200

    first commit

---
+++ src/main/java/com/exemple/ebankbackend/entities/CurrentAccount.java
@@ -0,0 +1,13 @@
+package com.exemple.ebankbackend.entities;
+
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Entity
+@DiscriminatorValue("CUR")
+@Data @AllArgsConstructor @NoArgsConstructor
+public class CurrentAccount extends BankAccount {
+    private double overDraft;
+}
---
+++ src/main/java/com/exemple/ebankbackend/enums/OperationType.java
@@ -0,0 +1,5 @@
+package com.exemple.ebankbackend.enums;
+
+public enum OperationType {
+    DEBIT, CREDIT
+}
